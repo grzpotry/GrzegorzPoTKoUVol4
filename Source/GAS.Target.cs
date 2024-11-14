f86f67598343d353932e882a1c55555f333c3e43
Grzegorz Potrykus <potrykus.g@evehor.com>
2024-11-14 21:22:11 +0100

    updated build settings

--- Source/GAS.Target.cs
+++ Source/GAS.Target.cs
@@ -8,7 +8,7 @@ public class GASTarget : TargetRules
 	public GASTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("GAS");
 	}
